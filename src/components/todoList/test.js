f4b41fca14b514e9a324bd42c6406b6cd934908d
Matthew M <mpm54@njit.edu>
2018-05-07 15:12:01 -0400

    Added todoList component and unit test

---
+++ src/components/todoList/test.js
@@ -0,0 +1,28 @@
+/* global describe, it, expect */
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import { configure } from 'enzyme';
+import TodoList from '.';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+describe('TodoList component', () => {
+    const todos = [
+        {
+            id: 1,
+            text: 'A todo',
+        },
+    ];
+
+    const component = shallow(<TodoList todos={todos} />);
+
+    it('Should render successfully', () => {
+        expect(component.exists()).toEqual(true);
+    });
+
+    it('Should display a todo when passed in as a prop', () => {
+        expect(component.find('.todo-text').text()).toEqual(todos[0].text);
+    });
+});
