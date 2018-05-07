4c4e63155b183f0fdd9d1de82be3176ff27e155d
Matthew M <mpm54@njit.edu>
2018-05-07 14:11:30 -0400

    Added redux and react-redux, created store, and edited src files for redux

--- src/App.test.js
+++ src/App.test.js
@@ -2,12 +2,21 @@
 import React from 'react';
 import { configure } from 'enzyme';
 import { shallow } from 'enzyme';
-import App from './App';
+import { App } from './App';
+import { initialState } from './reducers/';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 
 it('App renders without crashing', () => {
-    const component = shallow(<App />);
+    const mockFunction = jest.fn();
+
+    const component = shallow(
+        <App
+            state={initialState}
+            submitTodo={mockFunction}
+        />,
+    );
+
     expect(component.exists()).toEqual(true);
 });
