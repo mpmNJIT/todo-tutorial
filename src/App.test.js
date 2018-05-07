19333fb8c5b71d7778b98a38765541060190809f
Matthew M <mpm54@njit.edu>
2018-05-07 12:20:22 -0400

    Added addTodo component with unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,9 @@
 /* global it, expect */
 import React from 'react';
+import { configure } from 'enzyme';
 import { shallow } from 'enzyme';
 import App from './App';
+import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 
