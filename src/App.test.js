e797a3cc404d0b569d083f74c21d0571d8f082d7
Matthew M <mpm54@njit.edu>
2018-05-07 10:44:47 -0400

    Finished setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,6 @@
 /* global it, expect */
 import React from 'react';
-import { configure } from 'enzyme';
 import { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
 
 configure({ adapter: new Adapter() });
