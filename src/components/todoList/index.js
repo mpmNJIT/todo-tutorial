04505cefce4f199da8cf13ee28f798f24947acb2
Matthew M <mpm54@njit.edu>
2018-05-09 09:05:09 -0400

    Added pretty CSS stuff

--- src/components/todoList/index.js
+++ src/components/todoList/index.js
@@ -5,31 +5,37 @@ const TodoList = ({
   todos, deleteTodo, markTodo, editTodo,
 }) => {
   let input;
-  const editShow = { display: 'inline' };
   const todoItems = todos.map(todo => (
     <li
       className="todo-obj"
       key={todo.id}
       style={{ textDecoration: todo.marked ? 'line-through' : 'none' }}
     >
+      <span className="todo-text">
+        {todo.text}
+      </span>
 
-      <button
-        type="button"
-        className="todo-mark"
-        onClick={() => markTodo(todo.id)}
-      >
+      <div id="buttons-div" style={{ display: 'inline' }}>
+
+        <button
+          type="button"
+          className="todo-mark"
+          onClick={() => markTodo(todo.id)}
+        >
             Mark
-      </button>
+        </button>
 
-      <button
-        type="button"
-        className="todo-delete"
-        onClick={() => deleteTodo(todo.id)}
-      >
+        <button
+          type="button"
+          className="todo-delete"
+          onClick={() => deleteTodo(todo.id)}
+        >
                 Delete
-      </button>
+        </button>
 
-      <div id="edit-div" style={editShow}>
+      </div>
+
+      <div id="edit-div" style={{ display: 'inline' }}>
         <form
           onSubmit={(event) => {
                     event.preventDefault();
@@ -49,10 +55,6 @@ const TodoList = ({
           </button>
         </form>
       </div>
-
-      <span className="todo-text">
-        {todo.text}
-      </span>
     </li>
   ));
 
